ba4923bf5dc73b58bccd523bbc4326bf581ba791
konst77 <rayyoung.moon@gmail.com>
2025-07-13 20:16:08 -0700

    updated design aesthetics and copywriting, currently distinguishing motion within the layouts.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <Grids className="bg-black text-white">
       <div className="p-4 md:p-10">
-        <h1 className="text-[56px] md:text-[72px] leading-[1.15] uppercase">
+        <h1 className="text-[56px] md:text-[72px] leading-[1.15] uppercase text-[#333]">
           See you back
           <br />
           at the table.
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,13 @@ import Grids from "./components/grids";
 import OptimizedImage from "./components/OptimizedImage";
 import main from "@/public/img/main.svg"
 import dummy from "@/public/img/dummy-photo.jpg"
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       {/* Landing header */}
-      <Grids className="bg-black text-[#f1f1f5]">
-        <div className="flex flex-col items-start md:justify-end gap-4 md:pt-0 md:p-10 p-4 md:col-span-1">
+      <Grids className="bg-black text-[#fff]">
+        <div className="flex flex-col items-start gap-4 md:pt-0 md:p-10 p-4 md:col-span-1">
           <OptimizedImage
             src={main.src}
             height={40}
@@ -19,23 +17,11 @@ export default function Home() {
             className="image col-span-full w-full h-[40px]"
             priority={true}
           />
-          <h1 className="text-[48px] md:text-[56px] leading-[1.15]">
+          <h1 className="text-[48px] md:text-[56px] lg:text-[72px] leading-[1.15]">
             Recipes with soul.
             <br />
             Stories with flavor.
           </h1>
-          <p className="text-[#767676]">
-            Ray’s Table shares simple, healthy meals for everyday joy—served with kindness, a bit of humor, and always a story.
-          </p>
-          <div className="mt-6 hidden md:block">
-            <Button
-              size="lg"
-              className="bg-orange-500 text-white hover:bg-orange-400 hover:text-gray-100">
-              <Link href="/recipes">
-                <span>Visit Recipes</span>
-              </Link>
-            </Button>
-          </div>
         </div>
         {/*dummy photo*/}
         <OptimizedImage
@@ -48,91 +34,44 @@ export default function Home() {
       </Grids>
 
       {/* Value Proposition */}
-      <Grids className='bg-[#fff] text-[#000] border border-[#f1f1f5] md:h-[900px] md:p-10'>
-        <div className="p-4 md:p-10">
-          <h2 className="text-[40px] md:text-[48px] leading-[1.15]">Why Ray's Table?</h2>
-        </div>
-        <OptimizedImage
-          src={dummy.src}
-          alt=""
-          height={400}
-          width={1200}
-          className="w-full md:col-span-1 self-end"
-        />
-        <div className="flex flex-col gap-10 md:col-start-3 p-4 md:p-10">
-          <div className="flex flex-col gap-2">
-            <h3 className="text-[24px] md:text-[32px] md:leading-[1.5]">
-              <span className="text-orange-500">✦ </span>Storytelling first
-            </h3>
-            <p className="text-[16px] leading-[1.45] md:text-[18px] text-[#767676]">
-              Recipes that bring memories, feelings, and connection to the table.
-            </p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h3 className="text-[24px] md:text-[32px] md:leading-[1.5]">
-              <span className="text-orange-500">✦ </span>Simple & healthy
-            </h3>
-            <p className="text-[16px] leading-[1.45] md:text-[18px] text-[#767676]">
-              No stress. No fluff. Just feel-good food made accessible.
-            </p>
-          </div>
-          <div className="flex flex-col gap-2">
-            <h3 className="text-[24px] md:text-[32px] md:leading-[1.5]">
-              <span className="text-orange-500">✦ </span> Real, kind, and funny
-            </h3>
-            <p className="text-[16px] leading-[1.45] md:text-[18px] text-[#767676]">
-              You're not alone in the kitchen. We laugh, spill, and eat together.
-            </p>
+      <Grids className='bg-[#fff] text-[#000] md:h-[640px] md:p-10'>
+        <div className="flex flex-col gap-10 p-4 md:p-10 md:col-span-full self-center
+        text-[40px] md:text-[56px] lg:text-[80px] leading-[1.25]
+        ">
+          <div className="flex flex-col self-center text-center">
+            <h2 className="">
+              We tell beautiful stories,
+              <br />
+              focusing on simple and healthy meals,
+              <br />
+              and to see you feel and live your moment.
+            </h2>
           </div>
         </div>
       </Grids>
 
       {/* Recipe overlooking */}
-      <Grids className="bg-black text-[#fff]">
-        <div className="flex flex-row justify-between p-4 md:p-10">
-          <h2 className="text-[40px] md:text-[48px] leading-[1.15]">
-            To inspire you
-          </h2>
-        </div>
-
+      <Grids className="bg-white text-black md:mt-20">
         {/* Grid for featured recipes, make it dynamic? */}
-        <div className="md:col-span-2 md:grid md:grid-cols-2 md:pt-10 gap-0">
-          <OptimizedImage
-            src={dummy.src}
-            alt="dummy"
-            height={400}
-            width={600}
-            className=""
-          />
-          <OptimizedImage
-            src={dummy.src}
-            alt="dummy"
-            height={400}
-            width={600}
-            className=""
-          />
-          <OptimizedImage
-            src={dummy.src}
-            alt="dummy"
-            height={400}
-            width={600}
-            className=""
-          />
-          <OptimizedImage
-            src={dummy.src}
-            alt="dummy"
-            height={400}
-            width={600}
-            className=""
-          />
+        <div className="md:col-span-full p-4 md:p-10">
+          <p className="text-[40px] leading-[1.15]">
+            Just feel, read, and
+            <br />
+            create your dish.
+          </p>
+        </div>
+        <div className="md:col-span-3 flex flex-row gap-6">
+          <div className="h-[640px] w-full bg-[#f3f3f3]" />
+          <div className="h-[640px] w-full bg-[#f3f3f3]" />
+          <div className="h-[640px] w-full bg-[#f3f3f3]" />
         </div>
       </Grids>
 
       {/* About Ray */}
-      <Grids className="bg-[#fff] text-[#000]">
+      <Grids className="">
         <div className="p-4 flex flex-col gap-4 md:p-10">
           <h2 className="text-[40px] md:text-[48px] leading-[1.15]">
-            Little about Ray
+            Welcome to my table
           </h2>
           <p className="text-[#555]">
             Hi, I’m Ray. I cook, I feel things, and I believe food should make you feel a little more whole.
